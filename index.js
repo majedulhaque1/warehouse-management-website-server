ac9e4e839d01d8829e370bcf4551a11829fab6d1
majedulhaque1 <majedulhaquerakib601@gmail.com>
2022-05-01 08:05:45 +0600

    post api done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -21,7 +21,19 @@ async function run() {
         await client.connect();
 
         const carCollection = client.db('cardelars').collection('cars');
-
+        app.get('/additem/:Id', async (req, res) =>{
+            const id = req.params.Id;
+            const query = {_id : ObjectId(id)};
+            const result = await carCollection.findOne(query);
+            console.log(result);
+            res.send(result);
+        })
+        app.get('/additem', async (req, res) =>{
+            const query = {};
+            const cursor = carCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
         app.post('/additem', async (req, res) =>{
             const query = req.body;
             const result = await carCollection.insertOne(query);
